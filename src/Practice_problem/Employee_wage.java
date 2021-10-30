cf68c6199f22f54f76b8d070b7a43965bdc8ab4b
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-30 18:34:12 +0530

    Solving_Switch_Case_statement

--- src/Practice_problem/Employee_wage.java
+++ src/Practice_problem/Employee_wage.java
@@ -3,24 +3,26 @@ package Practice_problem;
 public class Employee_wage {
 	public static final int EMP_FULL_TIME_HOUR = 1;
     public static final int EMP_PART_TIME_HOUR = 2;
-	public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int EMP_WAGE_PER_HOUR = 20;
     public static void main(String[] args)
     {
-    	int empHours;
-        int empCheck = (int) (Math.floor(Math.random()*10)%3);
-        if ( empCheck == EMP_FULL_TIME_HOUR ){
-            empHours = 8;
-            System.out.println("Employee is Present");
+        int empHours;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        switch (empCheck) {
+        case EMP_FULL_TIME_HOUR -> {
+    empHours = 8;
+    System.out.println("Employee is present for full time");
         }
-        else if ( empCheck == EMP_PART_TIME_HOUR ){
-            empHours = 4;
-            System.out.println("Employee is Part time present");
+        case EMP_PART_TIME_HOUR -> {
+         empHours = 4;
+         System.out.println("Employee is present for half time");
         }
-        else {
-            empHours = 0;
-            System.out.println("Employee is Absent");
-        }
+        default -> {
+         empHours = 0;
+    	 System.out.println("Employee is absent");
+         }
+      }
         int empWage = (EMP_WAGE_PER_HOUR * empHours);
-        System.out.println("Employee Wage = " + empWage);  
-    }
+        System.out.println("Employee Wage = " + empWage);
+    }     
 }
